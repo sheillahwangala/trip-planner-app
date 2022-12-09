210bb2edd66dc6a7ed288ae1698829bb04ed927a
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-09 11:11:49 +0300

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+trip1 = Trip.create(
+   title: "Jamuhuri gate-away",
+   start_date: "Dec 11th 2022",
+    end_date: "Dec 12th 2022",
+    user_id: 1
+)
+
+destination1 = Destination.create(
+    name: "Serena Elementaita",
+    location: "Naivasha",
+    notes: "enjoy scenary, have fun",
+    trip_id: 1
+   
+)
