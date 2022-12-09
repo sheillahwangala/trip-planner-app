53fb671a5b263e88d1aa1ff2eecac06953cbecb6
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-09 15:35:30 +0300

    update

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,20 @@
 class DestinationsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        destinations = Destination.all
+        render json: destinations
+    end
+
+    def show
+        destination = Destination.find(params[:id])
+        render json: destination
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Destination not found" }, status: :not_found
+    end
+    
 end
