82956436bbc7b75ef24bb6acbabdec4fed0ce539
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-11 19:18:29 +0300

    update

--- config/application.rb
+++ config/application.rb
@@ -43,8 +43,6 @@ module TripPlanner
 
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
-
-    Zeitwerk::Loader.eager_load_all
     
   end
 end
